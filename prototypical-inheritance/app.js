4e9fcf5efc15b64462cfa992e153f1bb168e1654
Vardan Hakobyan <vardan_live@live.com>
2022-11-14 15:54:45 +0400

    feat: better prototypical inheritance

--- prototypical-inheritance/app.js
+++ prototypical-inheritance/app.js
@@ -1,4 +1,45 @@
 // Prototypical inheritance
+
+/*
+  Better approach without reassigning the `prototype` and the constructor of the child
+  Some context here - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain#building_longer_inheritance_chains
+  See "You may also see some legacy code using Object.create() to build ..." section.
+*/
+function Parent(arg) {
+	console.log('initializing...', arg);
+	this.name = 'parent'
+}
+Parent.prototype.printName = function() {
+	console.log('name is: ', this.name);
+}
+function Child(args) {
+	// In the child function’s constructor, call the parent’s constructor with “Object.call(...)”
+	// (pass any required parameter to it), so we will have the logic from parent’s constructor executed here.
+	Parent.call(this, args);
+	this.name = 'child';
+}
+Child.prototype.printCustomName = function() {
+	console.log('custom in child: ', this.name);
+}
+Object.setPrototypeOf(Child.prototype, Parent.prototype);
+
+function Child2(args) {
+	Child.call(this, args);
+	this.name = 'child 2';
+}
+Object.setPrototypeOf(Child2.prototype, Child.prototype);
+Child2.prototype.printCustomName2 = function() {
+	console.log('custom in child lvl2: ', this.name);
+}
+objParent = new Parent('parent argument');
+objChild = new Child('1st level child');
+objChild2 = new Child2('child lvl2 param');
+
+
+
+/*
+* Old approach (where the `prototype` and the constructor of the child are overwritten and then restored)
+* */
 function Class1() {
 	this.val = null;
 
